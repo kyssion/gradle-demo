b76d5e0605e9617d246acbac0360ac45a3608349
kyssion <1409915687@qq.com>
2019-02-10 17:49:09 +0800

    add-jar-library

---
+++ create-java-library/src/test/java/java/test/library/LibraryTest.java
@@ -0,0 +1,18 @@
+package java.test.library;
+
+
+
+import org.junit.Test;
+
+import java.library.Library;
+
+import static org.junit.Assert.*;
+
+
+public class LibraryTest {
+    @Test
+    public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
