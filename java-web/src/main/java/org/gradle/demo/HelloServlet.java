90b138fbb5513c34b4f25c60adb21fa9a11c318b
kys <1409915687@qq.com>
2019-02-14 11:20:59 +0800

    update

--- java-web/src/main/java/org/gradle/demo/HelloServlet.java
+++ java-web/src/main/java/org/gradle/demo/HelloServlet.java
@@ -17,7 +17,9 @@ public class HelloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String name = request.getParameter("name");
-        if (name == null) name = "World";
+        if (name == null) {
+            name = "World";
+        }
         request.setAttribute("user", name);
         request.getRequestDispatcher("response.jsp").forward(request, response);
     }
